c9e49c09d0df22716227075cad1121c8285f38ee
HyeonWooGa <yeonwoopark22@gmail.com>
2022-07-17 23:55:11 +0900

    React-Query 코드 추가

--- components/lecture/LectureList.tsx
+++ components/lecture/LectureList.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import LectureItem from "./LectureItem";
+import { useQuery } from "react-query";
 
 const LectureList = (): JSX.Element => {
-  return (
-    <div>
-      <LectureItem />
-    </div>
-  );
-};
+  const { isLoading, data } = useQuery("lecture_list", () => {
+    return fetch("/api/lectures")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        return res;
+      });
+  });
 
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  const lectureItems = data.lectureList.map((lecture) => {
+    return <LectureItem key={lecture.id} lecture={lecture} />;
+  });
+
+  return <div>{lectureItems}</div>;
+};
 export default LectureList;
